259891b083f021c30a640b2ee54b0f75716c4638
snazzlemcbazzle <richard.peckett@outlook.com>
2023-01-01 18:51:58 +0000

    add virtual here usb client service

--- GsScripts/scripts/manager.ps1
+++ GsScripts/scripts/manager.ps1
@@ -22,7 +22,7 @@ function Show-Menu
      .  _-~ . \                   /   ~-_     
      _-~       `\               /'*      ~-_  
     ~           /`--___   ___--'\           ~
-           *  /        ---     .  \   Fuck you Nvidia!
+           *  /        ---     .  \   Go f**k yourselves Nvidia.
             /     *     |           \
           /             |   *         \
                      .  |        .
@@ -32,11 +32,11 @@ function Show-Menu
 
     Write-Host $wow
     Write-Host
-    Write-Host "--- Sunshine Gamestream Server Admin ---"
+    Write-Host "--- Sunshine Gamestream Server and VirtualHere Client Admin ---"
     Write-Host
-    Write-Host "1: Press '1' to start Sunshine server."
-    Write-Host "2: Press '2' to stop Sunshine server."
-	Write-Host "3: Press '3' to open Sunshine Admin UI."
+    Write-Host "1: Press '1' to start Sunshine server and VirtualHere client."
+    Write-Host "2: Press '2' to stop Sunshine server and VirtualHere client."
+	Write-Host "3: Press '3' to open Sunshine Admin UI and VirtualHere client UI."
     Write-Host "Q: Press 'q' to quit."
 }
 
@@ -54,10 +54,11 @@ function Run-AsAdmin
 function Show-Service
 {
 	Write-Host
-    Write-Host "(getting Sunshine service status, please wait...)"
+    Write-Host "[getting Sunshine and VirtualHere service statuses, please wait...]"
     Start-Sleep -Seconds 3
     Write-Host
     Get-Service "sunshinesvc"
+	Get-Service "vhclient"
 }
 
 do
@@ -71,17 +72,20 @@ do
      {
          '1' {
 			 Write-Host "Starting Sunshine server..."
-			 Run-AsAdmin -ScriptPath 'start.ps1'
+			 Run-AsAdmin -ScriptPath 'sunrise.ps1'
+			 Run-AsAdmin -ScriptPath 'startvh.ps1'
          } 
          
          '2' {
 			 Write-Host "Stopping Sunshine server..."
-			 Run-AsAdmin -ScriptPath 'stop.ps1'
+			 Run-AsAdmin -ScriptPath 'sunset.ps1'
+			 Run-AsAdmin -ScriptPath 'stopvh.ps1'
          }
 		 
 		 '3' {
 			 Write-Host "Opening https://localhost:47990/"
 			 Start-Process "https://localhost:47990/"
+			 Start-Process "E:\Program Files\vhui64.exe"
          }
      }
  }
