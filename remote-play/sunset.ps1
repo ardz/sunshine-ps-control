3af5d8e260c909abb7a48ee8b310ffd094acffd2
RicardoDelFonzo <richard.peckett@googlemail.com>
2023-05-31 13:59:19 +0100

    Delete remote-play directory

--- remote-play/sunset.ps1
+++
@@ -1,7 +0,0 @@
-$sunshineservice = Get-Service | Where-Object {$_.Name -eq "SunshineService"}
-
-if ($sunshineservice.Status -eq "Running"){
-    
-    $sunshineservice | Stop-Service
-    Sleep 3
-}
